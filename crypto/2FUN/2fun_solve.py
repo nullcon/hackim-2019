c8561723a3357bdf41b629190e62b40382fc4ff9
Sudhakar Verma <svermaiiti@gmail.com>
2019-01-17 12:24:44 +0530

    Adding solution to too fun.

---
+++ crypto/2FUN/2fun_solve.py
@@ -0,0 +1,110 @@
+from hashlib import md5
+from binascii import hexlify, unhexlify
+BLOCK_LENGTH = 16
+KEY_LENGTH = 3
+ROUND_COUNT = 16
+
+sbox = [210, 213, 115, 178, 122, 4, 94, 164, 199, 230, 237, 248, 54,
+        217, 156, 202, 212, 177, 132, 36, 245, 31, 163, 49, 68, 107,
+        91, 251, 134, 242, 59, 46, 37, 124, 185, 25, 41, 184, 221,
+        63, 10, 42, 28, 104, 56, 155, 43, 250, 161, 22, 92, 81,
+        201, 229, 183, 214, 208, 66, 128, 162, 172, 147, 1, 74, 15,
+        151, 227, 247, 114, 47, 53, 203, 170, 228, 226, 239, 44, 119,
+        123, 67, 11, 175, 240, 13, 52, 255, 143, 88, 219, 188, 99,
+        82, 158, 14, 241, 78, 33, 108, 198, 85, 72, 192, 236, 129,
+        131, 220, 96, 71, 98, 75, 127, 3, 120, 243, 109, 23, 48,
+        97, 234, 187, 244, 12, 139, 18, 101, 126, 38, 216, 90, 125,
+        106, 24, 235, 207, 186, 190, 84, 171, 113, 232, 2, 105, 200,
+        70, 137, 152, 165, 19, 166, 154, 112, 142, 180, 167, 57, 153,
+        174, 8, 146, 194, 26, 150, 206, 141, 39, 60, 102, 9, 65,
+        176, 79, 61, 62, 110, 111, 30, 218, 197, 140, 168, 196, 83,
+        223, 144, 55, 58, 157, 173, 133, 191, 145, 27, 103, 40, 246,
+        169, 73, 179, 160, 253, 225, 51, 32, 224, 29, 34, 77, 117,
+        100, 233, 181, 76, 21, 5, 149, 204, 182, 138, 211, 16, 231,
+        0, 238, 254, 252, 6, 195, 89, 69, 136, 87, 209, 118, 222,
+        20, 249, 64, 130, 35, 86, 116, 193, 7, 121, 135, 189, 215,
+        50, 148, 159, 93, 80, 45, 17, 205, 95]
+p = [3, 9, 0, 1, 8, 7, 15, 2, 5, 6, 13, 10, 4, 12, 11, 14]
+
+
+invsbox = [0 for i in xrange(len(sbox))]
+
+for idx, val in enumerate(sbox):
+    invsbox[val] = idx
+
+invp = [0 for i in xrange(len(p))]
+
+for idx, val in enumerate(p):
+    invp[val] = idx
+
+
+def xor(a, b):
+    return bytearray(map(lambda s: s[0] ^ s[1], zip(a, b)))
+
+
+def unfun(key, ct):
+    assert len(ct) == BLOCK_LENGTH
+    assert len(key) == KEY_LENGTH
+    key = bytearray(unhexlify(md5(key).hexdigest()))
+    pt = bytearray(ct)
+    for _ in range(ROUND_COUNT):
+        nct = bytearray(BLOCK_LENGTH)
+        for i in range(BLOCK_LENGTH):
+            nct[i] = pt[invp[i]]
+        pt = nct
+        for i in range(BLOCK_LENGTH):
+            pt[i] = invsbox[pt[i]]
+        pt = xor(pt, key)
+    return hexlify(pt)
+
+def fun(key, pt):
+    assert len(pt) == BLOCK_LENGTH
+    assert len(key) == KEY_LENGTH
+    key = bytearray(unhexlify(md5(key).hexdigest()))
+    ct = bytearray(pt)
+    for _ in range(ROUND_COUNT):
+        ct = xor(ct, key)
+        for i in range(BLOCK_LENGTH):
+            ct[i] = sbox[ct[i]]
+        nct = bytearray(BLOCK_LENGTH)
+        for i in range(BLOCK_LENGTH):
+            nct[i] = ct[p[i]]
+        ct = nct
+    return hexlify(ct)
+
+def toofun(key, pt):
+    assert len(key) == 2 * KEY_LENGTH
+    key1 = key[:KEY_LENGTH]
+    key2 = key[KEY_LENGTH:]
+
+    ct1 = unhexlify(fun(key1, pt))
+    ct2 = fun(key2, ct1)
+
+    return ct2
+
+givenct = toofun('ABCDEF','A'*BLOCK_LENGTH)
+
+flagct = toofun('ABCDEF','B'*BLOCK_LENGTH)
+
+enctable = {}
+
+for i in xrange(256*256*256):
+    enctable[fun(unhexlify("{:06x}".format(i)), 'A'*16)] = i
+
+print "enctable done"
+
+dectable = {}
+
+for i in xrange(256*256*256):
+    dectable[unfun(unhexlify("{:06x}".format(i)), unhexlify(givenct))] = i
+
+print "dectable done"
+
+key = ''
+
+for i in enctable:
+    if i in dectable:
+        print enctable[i], dectable[i]
+        key = unhexlify("{:06x}".format(enctable[i]))+unhexlify("{:06x}".format(dectable[i]))
+
+print unhexlify(unfun(key[:3], unhexlify(unfun(key[3:], unhexlify(flagct)))))
