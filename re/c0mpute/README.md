d6c45771fd488f9d7720739bdf3226a1cc95d682
jaiverma <jai2.verma@outlook.com>
2019-01-31 23:29:34 +0530

    Updating c0mpute re challenge
    Updated the code to make bruteforcing the flag harder.
    Made the OpenCL kernel a little harder to reverse.

--- re/c0mpute/README.md
+++ re/c0mpute/README.md
@@ -8,18 +8,13 @@ The program takes input from user and runs an OpenCL kernel on that input. It th
 The transformation done is extremely trivial. We have a global alphabet array which stores `a-zA-Z0-9_`. Indices into this alphabet array are calculated from the user input. This array of indices then undergoes the simple transformation:
 
 ```c
-B[i] = ((A[i] + 4) * 3) - 9; // A is the input index int array
-B[i] = ((B[i] + 1) * 2) - 3;
-B[i] = ((B[i] + 7) * 5) - 1;
-```
-
-This kernel is opimised by the Intel OpenCL compiler to:
-```c
-B[i] = 30 * A[i] + 52
+B[i] = ((A[i] + 4.1) * 3.0) - 8.98;             // A is input alphabet index int array
+B[i] = ((B[i] + 1.03) * 1.998) - 3.201;
+B[i] = ((B[i] + 7.10345) * 5.0102) - 0.98912;
 ```
 
 This new array of indices `B` is used to calculate the string post transformation from the alphabet array. All indices are `idx % len(alphabet)`.
 
 Then a function compares this new string character by character for validation.
 
-The OpenCL kernel can be reversed by dumping the `.ocl.obj` section of the kernel and disassembling it. The script to do so is present in the `solution` folder.
+The OpenCL kernel can be reversed by dumping the `.ocl.obj` section of the kernel and disassembling it. The script to do so is present in the `solution` folder along with the ocl obj for the kernel used.
--- re/c0mpute/src/main.c
+++ re/c0mpute/src/main.c
@@ -8,6 +8,7 @@
 
 char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_'};
 int alphabet_size = 63;
+int flag_size = 21;
 
 unsigned int index_of(char a) {
     for (int i = 0; i < alphabet_size; i++)
@@ -17,9 +18,372 @@ unsigned int index_of(char a) {
 }
 
 int check_flag(char *s) {
-    if  ((s[0] == 'i') && (s[1] == '7') && (s[2] == 'l') && (s[3] == 'x') && (s[4] == 'l') && (s[5] == 'M') && (s[6] == 'D') && (s[7] == 'x') && (s[8] == 'S') && (s[9] == 'i') && (s[10] == '4') && (s[11] == 'D') && (s[12] == 'l') && (s[13] == '_') && (s[14] == 'D') && (s[15] == 'G') && (s[16] == '1') && (s[17] == '_') && (s[18] == '_') && (s[19] == '1') && (s[20] == 'A'))
-        return 0;
-    return 1;
+    //l _ p A p Q I A W m 8 I p c I K 4 c c 4 E
+    int solve[21];
+    for (int i = 0; i < flag_size; i++)
+        solve[i] = 0;
+
+    if (s[0] == 'l')
+        solve[0] = 1;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[1] == '_')
+        solve[1] = 2;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[2] == 'p')
+        solve[2] = 3;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[3] == 'A')
+        solve[3] = 4;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[4] == 'p')
+        solve[4] = 5;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[5] == 'Q')
+        solve[5] = 6;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[6] == 'I')
+        solve[6] = 7;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[7] == 'A')
+        solve[7] = 8;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[8] == 'W')
+        solve[8] = 9;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[9] == 'm')
+        solve[9] = 10;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[10] == '8')
+        solve[10] = 11;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[11] == 'I')
+        solve[11] = 12;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[12] == 'p')
+        solve[12] = 13;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[13] == 'c')
+        solve[13] = 14;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[14] == 'I')
+        solve[14] = 15;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[15] == 'K')
+        solve[15] = 16;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[16] == '4')
+        solve[16] = 17;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[17] == 'c')
+        solve[17] = 18;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[18] == 'c')
+        solve[18] = 19;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[19] == '4')
+        solve[19] = 20;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    if (s[20] == 'E')
+        solve[20] = 21;
+    else
+        __asm__(
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+            "nop;"
+        );
+    int sum = 0;
+    for (int i = 0; i < flag_size; i++)
+        sum += solve[i];
+    return (sum == ((flag_size * (flag_size + 1)) / 2));
 }
 
 int read_kernel(const char *kernel_name, char *kernel_buf) {
@@ -36,16 +400,15 @@ int read_kernel(const char *kernel_name, char *kernel_buf) {
 }
 
 int main(void) {
-    int flag_size = 21;
     char input_str[100];
     int input_digit = 0;
     memset(input_str, 0, 100 * sizeof(char));
     printf("Enter input:\n");
     fgets(input_str, 99, stdin);
     
-    int *A = (int*)malloc(sizeof(int) * flag_size);
+    float *A = (float*)malloc(sizeof(float) * flag_size);
     for (int i = 0; i < flag_size; i++)
-        A[i] = index_of(input_str[i]);
+        A[i] = (float)index_of(input_str[i]);
     
     char *kernel_alpha = (char*)malloc(MAX_SOURCE_SIZE);
     size_t alpha_size = read_kernel("obj/alpha.bin", kernel_alpha);
@@ -60,10 +423,10 @@ int main(void) {
     cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
     
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, flag_size * sizeof(int), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, flag_size * sizeof(int), NULL, &ret);
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, flag_size * sizeof(float), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, flag_size * sizeof(float), NULL, &ret);
     
-    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, flag_size * sizeof(int), A, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, flag_size * sizeof(float), A, 0, NULL, NULL);
     
     cl_program alpha = clCreateProgramWithBinary(context, 1, &device_id, &alpha_size, (const unsigned char**)&kernel_alpha, NULL, &ret);
     ret = clBuildProgram(alpha, 1, &device_id, NULL, NULL, NULL);
@@ -75,12 +438,15 @@ int main(void) {
     size_t global_item_size = flag_size;
     ret = clEnqueueNDRangeKernel(command_queue, alpha_bin, 1, NULL, &global_item_size, NULL, 0, NULL, NULL);
     
-    int *B = (int*)malloc(sizeof(int) * flag_size);
-    ret = clEnqueueReadBuffer(command_queue, b_mem_obj, CL_TRUE, 0, flag_size * sizeof(int), B, 0, NULL, NULL);
+    float *B = (float*)malloc(sizeof(float) * flag_size);
+    ret = clEnqueueReadBuffer(command_queue, b_mem_obj, CL_TRUE, 0, flag_size * sizeof(float), B, 0, NULL, NULL);
     
     char *C = (char*)malloc(sizeof(char) * flag_size + 1);
-    for (int i = 0; i < flag_size; i++)
-        C[i] = alphabet[B[i] % alphabet_size];
+    int temp = 0;
+    for (int i = 0; i < flag_size; i++) {
+        temp = (int)B[i];
+        C[i] = alphabet[temp % alphabet_size];
+    }
     C[flag_size] = '\0';
     
     ret = clFlush(command_queue);
@@ -98,7 +464,7 @@ int main(void) {
     int success = check_flag(C);
     free(C);
     
-    if (success == 0)
+    if (success)
         return 0;
     else
         return 1;
